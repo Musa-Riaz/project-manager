d196d1ee0981f3567ba09fb9c1adc84cf2274495
Musariaz <riazmusa69@gmail.com>
2025-06-15 15:12:28 +0500

    a

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,6 +36,6 @@ app.use("/teams", teamRoutes);
 // server
 const port = Number(process.env.PORT || 5000);
 
-app.listen(port,"0.0.0.0" ,() => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
